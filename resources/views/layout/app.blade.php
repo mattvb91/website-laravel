a4ae3ac72b299643252d555b1cea90cb57079007
Matt von Bargen <matt@mavon.ie>
2015-12-18 23:02:45 +0000

    basic styles

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,11 +6,19 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href="/css/sticky-footer.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         @include('errors.list')
         @yield('content')
+
+        <footer class="footer">
+            <hr/>
+            <div class="container">
+                <p class="text-muted"></p>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
