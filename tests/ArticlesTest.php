58249daa64765a56b5dbc993f7efe8b16f9fb638
Matt von Bargen <matt@mavon.ie>
2015-12-19 14:39:11 +0000

    updating article tests

--- tests/ArticlesTest.php
+++ tests/ArticlesTest.php
@@ -27,4 +27,27 @@ class ArticlesTest extends TestCase
 
         $this->assertInstanceOf(\App\Models\User::class, $article->user);
     }
+
+    public function testIndex()
+    {
+        factory(Article::class, 20)->create();
+
+        $this->get('/article')
+            ->seeStatusCode(200)
+            ->see('Articles');
+
+        $this->get('/article?page=2')
+            ->seeStatusCode(200)
+            ->see('Articles');
+    }
+
+    public function testShow()
+    {
+        factory(Article::class)->create();
+
+        $article = Article::first();
+        $this->get('/article/' . $article->getKey())
+            ->seeStatusCode(200)
+            ->see($article->getTitle());
+    }
 }
