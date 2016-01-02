4546db9504c6f1550194b935700723d54bf84bb3
Matt von Bargen <matt@mavon.ie>
2016-01-02 14:06:18 +0000

    fixing error

--- resources/assets/js/include.js
+++ resources/assets/js/include.js
@@ -1,8 +1,10 @@
 $('#flash-overlay-modal').modal();
 $('div.alert').not('.alert-important').delay(3000).slideUp();
 
-GitHubActivity.feed({
-    username: "mattvb91",
-    selector: "#feed",
-    limit: 10
-});
+if (typeof GitHubActivity !== 'undefined') {
+    GitHubActivity.feed({
+        username: "mattvb91",
+        selector: "#feed",
+        limit: 10
+    });
+}
